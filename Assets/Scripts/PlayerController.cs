c1b0f6dbdeaeac5c232ede2ffaee3906cebd6a60
ManelNeon <neonextra0@gmail.com>
2024-02-27 10:25:46 +0000

    Lesson 1.4
    Acabei por melhorar um pouco o código que fiz  criando uma class para meter o código mais bonito, também no playerfollow, meti uma linha para o a câmara olhar para o jogador.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("PlayerMovement")]
+
     [SerializeField] float speed = 20;
+
+    [SerializeField] float turnSpeed = 45;
+
+    float horizontalInput;
+
+    float verticalInput;
+
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //Control the speed of the Player, forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        //Control the rotation of the Player, sides
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
--- Assets/Scripts/PlayerFollow.cs
+++ Assets/Scripts/PlayerFollow.cs
@@ -6,13 +6,17 @@ public class PlayerFollow : MonoBehaviour
 {
     /// <Justificação>
     /// No tutorial dizem para meter a câmara a seguir o jogador e colocar uma variável offset, na minha opinião, fica melhor ter um empty dentro do jogador que tem a 
-    /// localização que pretendo para a câmara e meter apenas a posição igual a esse empty object.
+    /// localização que pretendo para a câmara e meter apenas a posição igual a esse empty object. Acaba por ter o mesmo efeito mas com um código mais simples e melhor.
     /// </Justificação>
     
     [SerializeField] GameObject cameraSpot;
 
+    [SerializeField] GameObject player;
+
     void LateUpdate()
     {
         this.transform.position = cameraSpot.transform.position;
+
+        transform.LookAt(player.transform);
     }
 }
